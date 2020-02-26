baf20549856493318ec0b0a7cc8663a33e9fba1a
Alisson Youssf <youssfbr@gmail.com>
2020-02-26 17:50:01 -0300

    Criando Componente Edição de Curso

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,16 +1,22 @@
+import { CourseService } from './course.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
 
 @Component({
   templateUrl: './course-info.component.html'
 })
 export class CourseInfoComponent implements OnInit {
 
-  courseId: number;
+  course: Course;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
 
   ngOnInit(): void {
-    this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+  }
+
+  save(): void {
+    this.courseService.save(this.course);
   }
 }

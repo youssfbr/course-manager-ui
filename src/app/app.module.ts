185699592e2b7d18d436bbf10cd84fbde6576b79
Alisson Youssf <youssfbr@gmail.com>
2020-02-29 11:01:39 -0300

    Segregando a aplicação - finalizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CourseModule } from './courses/course.module';
-import { Error404Component } from './error-404/error-404.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +19,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,19 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { Error404Component } from './component/error-404/error-404.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
-    RouterModule
+    RouterModule.forChild([
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   exports: [
     NavBarComponent
